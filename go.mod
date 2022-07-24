6763e67e362e2964eaa5c8c46c15b542d8135abd
Harry Thomas <harry@infosechoudini.com>
2022-07-24 17:30:16 -0400

    added better bacnet parsing to asset id

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require github.com/influxdata/telegraf v1.23.2
 
 require (
 	collectd.org v0.5.0 // indirect
+	github.com/766b/go-outliner v0.0.0-20180511142203-fc6edecdadd7 // indirect
 	github.com/BurntSushi/toml v0.4.1 // indirect
 	github.com/alecthomas/participle v0.4.1 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/caio/go-tdigest v3.1.0+incompatible // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
@@ -26,9 +29,12 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/prometheus/client_golang v1.12.2 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/prometheus/prometheus v1.8.2-0.20210430082741-2a4b8e12bbf2 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/sleepinggenius2/gosmi v0.4.4 // indirect
 	github.com/tidwall/gjson v1.14.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
@@ -36,8 +42,16 @@ require (
 	github.com/tinylib/msgp v1.1.6 // indirect
 	github.com/vjeantet/grok v1.0.1 // indirect
 	github.com/wavefronthq/wavefront-sdk-go v0.9.11 // indirect
+	github.com/weppos/publicsuffix-go v0.4.0 // indirect
+	github.com/zmap/rc2 v0.0.0-20131011165748-24b9757f5521 // indirect
+	github.com/zmap/zcrypto v0.0.0-20200508204656-27de22294d44 // indirect
+	github.com/zmap/zflags v1.4.0-beta.1 // indirect
+	github.com/zmap/zgrab2 v0.1.7 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- plugins/inputs/bacnet/bacnet.go
+++ plugins/inputs/bacnet/bacnet.go
@@ -1,46 +1,20 @@
 package bacnet
 
 import (
-	"encoding/binary"
-	"fmt"
-	"log"
 	"net"
+	"reflect"
+	"strconv"
 	"time"
-	"unsafe"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
 	"github.com/influxdata/telegraf/plugins/inputs"
+	zbacnet "github.com/zmap/zgrab2/modules/bacnet"
 )
 
-var socketPort net.PacketConn
-var onceExecute = false
-var invokeid uint8 = 255
-
-type BVLCpacket struct {
-	recvaddr string
-	typet    uint8
-	function uint8
-	length   uint16
-}
-type NPDUpacket struct {
-	version uint8
-	control uint8
-}
-type APDUpacket struct {
-	type_flags     uint8
-	invokeId       uint8
-	serviceChoice  uint8
-	objectIdTag    uint8
-	objectId       uint32
-	ProperityIdTag uint8
-	ProperityId    uint8
-	dataStartTag   uint8
-	applicationTag uint8
-
-	dataEndTag uint8
-}
 type requestInfo struct {
-	dstaddr     string //"ip:port"
+	dstaddr     string //"ip"
+	dstport     int
 	objectId    uint32 //obTy<<22 | obInst
 	ProperityId uint8
 	parmName    string
@@ -51,7 +25,7 @@ func (b *requestInfo) SampleConfig() string {
 	return `
 	[[inputs.BACnet]]
 	[[inputs.BACnet.Properity.Tags]]
-	dstaddr = "192.168.5.32:56419"
+	dstaddr = "192.168.5.32:47808"
 	objectId = 8388608
 	requestInfo.ProperityId = 85
 	parmName = "name of parameter"
@@ -71,187 +45,53 @@ func (b *requestInfo) Description() string {
 }
 
 func init() {
-	// do {
-	// 	socketPort, err := net.ListenPacket("udp", ":4788")
-	// }while(err!=nil)
-	// go GetDataUDP(socketPort)
-
 	inputs.Add("bacnet", func() telegraf.Input { return &requestInfo{} })
 }
 
-func (b *requestInfo) Gather(acc telegraf.Accumulator) string {
-	if onceExecute == false { //from init
-		for {
-			socketPort, err := net.ListenPacket("udp", ":4788")
-			if err != nil {
-				break
-			}
-		}
-		go GetDataUDP(socketPort, acc)
-
-		onceExecute = true
-	}
-
-	SendReadRequest(socketPort, b.dstaddr, b.objectId, b.ProperityId)
-	time.Sleep(time.Duration(b.waitTime * time.Millisecond))
-}
-
-func SendReadRequest(pc net.PacketConn, dstadr string, object uint32, properity uint8) {
-	if invokeid == 255 {
-		invokeid = 0
-	} else {
-		invokeid++
-	}
-	var data = [17]uint8{0x81, 0x0a, 0x00, 0x11, 0x01, 0x04, 0x00, 0x05, invokeid, 0x0c, 0x0c, uint8(object >> 24 & 0xff), uint8(object >> 16 & 0xff), uint8(object >> 8 & 0xff), uint8(object & 0xff), 0x19, properity}
-
-	LocalAddr, err := net.ResolveUDPAddr("udp", dstadr)
+func (b *requestInfo) Gather(acc telegraf.Accumulator) error {
+	conn, err := net.Dial("udp", b.dstaddr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	pc.WriteTo(data[:17], LocalAddr)
+
+	defer conn.Close()
+
+	b.QueryDevice(conn, acc)
+
+	return nil
 }
 
-func GetDataUDP(pc net.PacketConn, acc telegraf.Accumulator) {
-	for {
-		rcvbuf := make([]byte, 1472)
-		n, addr, err := pc.ReadFrom(rcvbuf)
-		if err != nil || n < 6 {
-			continue
-		}
+func (b *requestInfo) QueryDevice(conn net.Conn, acc telegraf.Accumulator) {
 
-		fmt.Printf("%v   ", addr)
-		for i := 0; i < n; i++ {
-			fmt.Printf("%02x ", rcvbuf[i])
-		}
-		fmt.Println("")
+	grouper := metric.NewSeriesGrouper()
 
-		var BVLCrcv BVLCpacket
-		var NPDUrcv NPDUpacket
-		BVLCrcv.recvaddr = fmt.Sprintf("%v", addr)
-		BVLCrcv.typet = rcvbuf[0]
-		BVLCrcv.function = rcvbuf[1]
-		BVLCrcv.length = uint16(rcvbuf[2]<<8 | rcvbuf[3])
-		NPDUrcv.version = rcvbuf[4]
-		NPDUrcv.control = rcvbuf[5]
+	log := new(zbacnet.Log)
 
-		if (BVLCrcv.recvaddr != requestInfo.dstaddr) || (BVLCrcv.typet != 0x81) || (BVLCrcv.function != 0x0a) || (BVLCrcv.length != uint16(n)) || (NPDUrcv.version != 1) || (NPDUrcv.control != 0) {
-			continue
-		}
+	t := reflect.TypeOf(log)
 
-		go DecodeAPDU(rcvbuf[:n], acc)
-	}
-}
-
-func DecodeAPDU(buf []uint8, acc telegraf.Accumulator) {
-	var APDUrcv APDUpacket
-	var count uint8 = 6
-	var data_length uint8 = 0
-	fields := make(map[string]interface{})
-	tags := make(map[string]string)
-	APDUrcv.type_flags = buf[count]
-	count++
-	APDUrcv.invokeId = buf[count]
-	count++
-	APDUrcv.serviceChoice = buf[count]
-	count++
-	APDUrcv.objectIdTag = buf[count]
-	count++
-	APDUrcv.objectId = uint32(uint32(buf[count])<<24 | uint32(buf[count+1])<<16 | uint32(buf[count+2])<<8 | uint32(buf[count+3]))
-	count += 4
-	APDUrcv.ProperityIdTag = buf[count]
-	count++
-	APDUrcv.ProperityId = buf[count]
-	count++
-	APDUrcv.dataStartTag = buf[count]
-	count++
-	APDUrcv.applicationTag = buf[count]
-	count++
-
-	if (APDUrcv.type_flags != 0x30) || (APDUrcv.invokeId != invokeid) || (APDUrcv.serviceChoice != 0x0c) || (APDUrcv.objectIdTag != 0x0c) || (APDUrcv.objectId != requestInfo.objectId) || (APDUrcv.ProperityIdTag != 0x19) || (APDUrcv.ProperityId != requestInfo.ProperityId) || (APDUrcv.dataStartTag != 0x3e) || (APDUrcv.applicationTag&0x08 != 0) {
-		return
+	for i := 0; i < t.NumMethod(); i++ {
+		method := t.Method(i)
+		reflect.ValueOf(log).MethodByName(method.Name).Call(nil)
 	}
 
-	if (APDUrcv.applicationTag & 0x07) < 5 {
-		data_length = APDUrcv.applicationTag & 0x07
-	} else if APDUrcv.applicationTag&0x07 == 5 {
-		data_length = buf[count]
-		count++
-	} else {
-		return
+	rtags := map[string]string{
+		"name":            log.ObjectName,
+		"location":        log.Location,
+		"instance_number": strconv.FormatUint(uint64(log.InstanceNumber), 10),
+		"description":     log.Description,
 	}
 
-	switch APDUrcv.applicationTag >> 4 {
-	case 1: //bool
-		if buf[count] != 0x3f {
-			return
-		}
-		fields["DeviceAddress"] = requestInfo.dstaddr
-		if data_length == 0 {
-			tags[requestInfo.parmName] = false
-			//fmt.Println(requestInfo.parmName, ": ", "false")
-		} else {
-			tags[requestInfo.parmName] = true
-			//fmt.Println(requestInfo.parmName, ": ", "true")
-		}
-		acc.AddFields("bacnet", fields, tags)
-		break
-	case 2: //unsigned int
-		var data_uint uint64 = 0
-		for i := 0; i < int(data_length); i++ {
-			data_uint = ((data_uint << 8) | uint64(buf[count]))
-			count += 1
-		}
-		if buf[count] != 0x3f {
-			return
-		}
-		fields["DeviceAddress"] = requestInfo.dstaddr
-		tags[requestInfo.parmName] = data_uint
-		acc.AddFields("bacnet", fields, tags)
-		//fmt.Println(requestInfo.parmName, ": ", data_uint)
-		break
-	case 4: //real
-		if data_length == 4 { //float
-			data_arr := []byte{buf[count], buf[count+1], buf[count+2], buf[count+3]}
-			count += 4
-			if buf[count] != 0x3f {
-				return
-			}
-			data_int := binary.BigEndian.Uint32(data_arr)
-			data_float := *(*float32)(unsafe.Pointer(&data_int))
-			fields["DeviceAddress"] = requestInfo.dstaddr
-			tags[requestInfo.parmName] = data_float
-			acc.AddFields("bacnet", fields, tags)
-			// fmt.Println(requestInfo.parmName, ": ", data_float)
-		} else if data_length == 8 { //double
-			data_arr := []byte{buf[count], buf[count+1], buf[count+2], buf[count+3], buf[count+4], buf[count+5], buf[count+6], buf[count+7]}
-			count += 8
-			if buf[count] != 0x3f {
-				return
-			}
-			data_int := binary.BigEndian.Uint64(data_arr)
-			data_float := *(*float64)(unsafe.Pointer(&data_int))
-			fields["DeviceAddress"] = requestInfo.dstaddr
-			tags[requestInfo.parmName] = data_float
-			acc.AddFields("bacnet", fields, tags)
-			// fmt.Println(requestInfo.parmName, ": ", data_float)
-		}
-		break
-	case 7: //string.
-		var RxString [256]string
-		for i := 0; i < int(data_length); i++ {
-			RxString[i] = string(buf[count])
-			RxString[i+1] = string(0)
-			count += 1
-		}
-		if buf[count] != 0x3f {
-			return
-		}
-		fields["DeviceAddress"] = requestInfo.dstaddr
-		tags[requestInfo.parmName] = RxString
-		acc.AddFields("bacnet", fields, tags)
-		//fmt.Println(requestInfo.parmName, ": ", RxString)
-		break
-	default: //other
-		return
+	rfields := map[string]string{
+		"vendor_id":        strconv.FormatUint(uint64(log.VendorID), 10),
+		"vendor_name":      log.VendorName,
+		"firmware_version": log.FirmwareRevision,
+		"app_version":      log.ApplicationSoftwareRevision,
+	}
+
+	grouper.Add("bacnet", rtags, time.Now(), "asset_id", rfields)
+
+	// Add the metrics grouped by series to the accumulator
+	for _, x := range grouper.Metrics() {
+		acc.AddMetric(x)
 	}
 }
